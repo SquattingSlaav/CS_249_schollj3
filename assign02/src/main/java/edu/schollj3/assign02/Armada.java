5b9192cc43553181a4cd28850cfa43df3482822e
SquattingSlaav <jahseh.onfroy.n@gmail.com>
2024-09-17 19:55:33 -0400

    Armada created

--- assign02/src/main/java/edu/schollj3/assign02/Armada.java
+++ assign02/src/main/java/edu/schollj3/assign02/Armada.java
@@ -1,7 +1,27 @@
 package edu.schollj3.assign02;
 
+import java.util.*;
+
 public class Armada {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        SpaceVessel vessel = new SpaceVessel();
+
+        System.out.println("Enter vessel name:");
+        String name = sc.nextLine();
+
+        System.out.println("Enter length and weight:");
+        String line = sc.nextLine();
+        Scanner parseLine = new Scanner(line);
+
+        int ft = parseLine.nextInt();
+        int lbs = parseLine.nextInt();
+
+        vessel.setName(name);
+        vessel.setLength(ft);
+        vessel.setWeight(lbs);
+
+        System.out.println(vessel);
     }
 }
