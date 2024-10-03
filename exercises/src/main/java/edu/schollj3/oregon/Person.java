ebd9f639b1a79afb6760e804b7c73c0f3f2596c0
SquattingSlaav <jahseh.onfroy.n@gmail.com>
2024-10-03 14:13:24 -0400

    wow

---
+++ exercises/src/main/java/edu/schollj3/oregon/Person.java
@@ -0,0 +1,32 @@
+package edu.schollj3.oregon;
+
+public class Person {
+    private String name;
+    private boolean alive = true;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public boolean kill() {
+        boolean success = alive;
+        alive = false;
+        return success;
+    }
+
+    public String toString() {
+        String s = name;
+        if(!alive) {
+            s += " (Deceased)";
+        }
+        return s;
+    }
+}
